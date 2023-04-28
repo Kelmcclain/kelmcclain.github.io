e8f4af5f3820cb8ac64a73c729225e7d8319cca2
McClain Kelvin <mcclainkel@gmail.com>
2023-04-28 17:45:38 +0300

    update

--- js/resumejs.js
+++ js/resumejs.js
@@ -1,14 +1,36 @@
-let number = document.getElementById("number");
-console.log(number)
-let counter = 0;
 
-setInterval(() => {
+htmlSkill();
+cssSkill() ;
+function htmlSkill() {
+    let number = document.getElementById("html-number");
+    console.log(number)
+    let counter = 0;
 
-    if(counter=== 85) {
-        clearInterval();
-    } else {
-        counter += 1;
-    number.innerHTML = counter + "%"
-    }
-    
-}, 7)
+    setInterval(() => {
+
+        if (counter === 85) {
+            clearInterval();
+        } else {
+            counter += 1;
+            number.innerHTML = counter + "%"
+        }
+
+    }, 10)
+}
+
+function cssSkill() {
+    let number = document.getElementById("css-number");
+    console.log(number)
+    let counter = 0;
+
+    setInterval(() => {
+
+        if (counter === 65) {
+            clearInterval();
+        } else {
+            counter += 1;
+            number.innerHTML = counter + "%"
+        }
+
+    }, 10)
+}
