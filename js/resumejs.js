c40c7559e67ce108563cbdd7a5fa56860e0b38d3
McClain Kelvin <mcclainkel@gmail.com>
2023-04-29 14:32:48 +0300

    jsupdate

--- js/resumejs.js
+++ js/resumejs.js
@@ -1,73 +1,76 @@
+let skillsArray = [];
 
-htmlSkill();
-cssSkill() ;
-jsSkill();
-flutterSkill();
-function htmlSkill() {
-    let number = document.getElementById("html-number");
-    console.log(number)
-    let counter = 0;
-
-    setInterval(() => {
-
-        if (counter === 85) {
-            clearInterval();
-        } else {
-            counter += 1;
-            number.innerHTML = counter + "%"
-        }
-
-    }, 8)
+class Skill {
+    constructor(skillname, level) {
+        this.skillname = skillname;
+        this.level = level;
+    }
 }
 
-function cssSkill() {
-    let number = document.getElementById("css-number");
-    console.log(number)
-    let counter = 0;
+const htmlskill = new Skill('html', 85);
+const cssskill = new Skill('css', 65);
+const jsskill = new Skill('javascript', 50);
+const flutterskill = new Skill('flutter', 35);
+//   const pythonskill = new Skill('html', 85);
 
-    setInterval(() => {
+skillsArray.push(htmlskill, cssskill, jsskill, flutterskill)
+let htmllevel = document.getElementById("html-number");
+let csslevel = document.getElementById("css-number");
+let jslevel = document.getElementById("js-number");
+let flutterlevel = document.getElementById("flutter-number");
 
-        if (counter === 65) {
-            clearInterval();
-        } else {
-            counter += 1;
-            number.innerHTML = counter + "%"
-        }
 
+
+skillsArray.forEach((skill, index)=> {
+    if (skill.skillname === 'html') {
+        let counter = 0;
+        setInterval(() => {
+            if (counter === skill.level) {
+                clearInterval();
+            } else {
+                counter += 1;
+                htmllevel.innerHTML = counter + "%"
+            }
     }, 8)
-}
-
-function jsSkill() {
-    let number = document.getElementById("js-number");
-    console.log(number)
-    let counter = 0;
-
-    setInterval(() => {
-
-        if (counter === 50) {
-            clearInterval();
-        } else {
-            counter += 1;
-            number.innerHTML = counter + "%"
-        }
-
+    } else if (skill.skillname === 'css') {
+        let counter = 0;
+        setInterval(() => {
+            if (counter === skill.level) {
+                clearInterval();
+            } else {
+                counter += 1;
+                csslevel.innerHTML = counter + "%"
+            }
     }, 8)
-}
-
-
-function flutterSkill() {
-    let number = document.getElementById("flutter-number");
-    console.log(number)
-    let counter = 0;
-
-    setInterval(() => {
-
-        if (counter === 35) {
-            clearInterval();
-        } else {
-            counter += 1;
-            number.innerHTML = counter + "%"
-        }
-
+    } else if (skill.skillname === 'css') {
+        let counter = 0;
+        setInterval(() => {
+            if (counter === skill.level) {
+                clearInterval();
+            } else {
+                counter += 1;
+                csslevel.innerHTML = counter + "%"
+            }
     }, 8)
-}
+    } else if (skill.skillname === 'javascript') {
+        let counter = 0;
+        setInterval(() => {
+            if (counter === skill.level) {
+                clearInterval();
+            } else {
+                counter += 1;
+                jslevel.innerHTML = counter + "%"
+            }
+    }, 8)
+    }  else if (skill.skillname === 'flutter') {
+        let counter = 0;
+        setInterval(() => {
+            if (counter === skill.level) {
+                clearInterval();
+            } else {
+                counter += 1;
+                flutterlevel.innerHTML = counter + "%"
+            }
+    }, 8)
+    } 
+})
